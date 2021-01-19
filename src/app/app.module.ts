b2c3251158061a19c2430f7982b6716f1be0479d
ErianaEsquivel <19170089@uttcampus.edu.mx>
2021-01-19 17:45:02 -0600

    Tratando de hacer an imaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
+import { OpenCloseComponent } from './open-close/open-close.component';
+import { PruebaComponent } from './prueba/prueba.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OpenCloseComponent,
+    PruebaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
